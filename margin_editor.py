2d6222c115177cc97c7e85a77e3e2941ad8c9af2
Chijioke Kamanu <chijioke.kamanu@bison.howard.edu>
2019-08-30 18:56:23 +0000

    Added some more functionality to the margin editor script

--- margin_editor.py
+++ margin_editor.py
@@ -3,6 +3,7 @@ import os
 def obtainMargins():
     left_margin = input("Enter a value for the left margin: ")
     right_margin = input("Enter a value for the right margin: ")
+    print()
     return (left_margin, right_margin)
 
 def printPageHeader():
@@ -13,5 +14,29 @@ def printPageHeader():
     return
 
 def main():
-    input = open("input_file.txt", r)
+    input_ = open("input_file.txt", "r") 
+    file_line_length = 80
     selected_margins = obtainMargins()
+    file_line = input_.readline()
+    right_padding = " " * int(selected_margins[0])
+    left_padding = " " * int(selected_margins[1])
+    file_line_length = 80 - int(selected_margins[0]) - int(selected_margins[1]) - len(file_line)
+    if file_line_length >= 0:
+        file_line = right_padding + file_line + left_padding
+    else:
+        available_line_length = 80 - (int(selected_margins[0]) + int(selected_margins[1]))
+        counter = 0
+        file_line_words = file_line.split()
+        file_line = right_padding + file_line_words[counter] + " "
+        available_line_length -= (1 + len(file_line_words[counter]))
+        while available_line_length >= 0:
+            counter += 1
+            file_line += file_line_words[counter] + " "
+            available_line_length -= 1 + len(file_line_words[counter])
+        file_line += left_padding
+    printPageHeader()
+    print(file_line)
+    print(file_line_length)
+    return 
+
+main()
